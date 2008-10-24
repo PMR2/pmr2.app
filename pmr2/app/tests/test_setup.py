b52a7ad2f51e20f4455ad1e11063c80b7687cd6e
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2008-10-24 04:39:44 +0000

    Code reorganization.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@2817 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/app/tests/test_setup.py
+++ pmr2/app/tests/test_setup.py
@@ -12,13 +12,20 @@ class TestProductInstall(TestCase):
             'ExposureContainer': None,
             'Workspace': None,
             'Sandbox': None,
-            'Exposure': None,
+            'Exposure': 'exposure_workflow',
         }
 
     def testTypesInstalled(self):
         for t in self.types.keys():
             self.failUnless(t in self.portal.portal_types.objectIds(),
-                            '%s content type not installed' % t)
+                            '`%s` content type not installed' % t)
+
+    def testWorkflowsInstalled(self):
+        for k, wf in self.types.iteritems():
+            if wf:
+                self.failUnless(wf in self.portal.portal_workflow.objectIds(),
+                                'workflow `%s` not installed for `%s`' % 
+                                    (wf, k))
 
 
 def test_suite():
