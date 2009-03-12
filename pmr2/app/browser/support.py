0952c6fa6516e96c54eee341c5b4db961cb98fdc
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2009-03-12 04:24:48 +0000

    Preliminary searches using the search object implemented.
    Progress towards:
    https://tracker.physiomeproject.org/show_bug.cgi?id=1597
    
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@3213 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/app/browser/support.py
+++ pmr2/app/browser/support.py
@@ -1,4 +1,5 @@
 import zope.component
+from zope.publisher.interfaces import NotFound
 
 import z3c.form.field
 import z3c.form.form
@@ -7,6 +8,8 @@ from plone.app.z3cform import layout
 
 from pmr2.app.interfaces import IPMR2Search, IPMR2SearchAdd
 from pmr2.app.content import PMR2Search
+
+from Products.CMFCore.utils import getToolByName
 
 import form
 import page
@@ -59,6 +62,43 @@ class PMR2SearchPage(page.TraversePage):
         'pmr2search.pt')
 
     def __call__(self, *args, **kwargs):
+
+        def clean_searchterm():
+            if not hasattr(self, 'searchterm') or self.searchterm is None:
+                return False
+            self.searchterm = self.searchterm.replace(' ', '_')
+            return True
+
+        def build_results():
+            d = {}
+            d[self.context.catalog_index] = self.searchterm
+            self.all_results = pt(**d)
+            if not self.all_results:
+                raise NotFound(self.context, self.context.title_or_id(),
+                               self.request)
+
+        def build_terms():
+            try:
+                self.all_terms = \
+                    pt.Indexes[self.context.catalog_index].uniqueValues()
+            except NotImplementedError:
+                pass
+
+        pt = getToolByName(self.context, 'portal_catalog')
+        if self.context.catalog_index not in pt.Indexes:
+            # Nothing is set.
+            return
+
+        self.catalog_index = self.context.catalog_index
+        self.searchterm = None
+        if 'request_subpath' in self.request:
+            self.searchterm = '/'.join(self.request['request_subpath'])
+
+        # now call the methods to set the variables for the template.
+        if clean_searchterm():
+            build_results()
+        else:
+            build_terms()
         return self.filetemplate()
 
 PMR2SearchPageView = layout.wrap_form(
