38ae2e0fcc93cbfb53ba957c073eecfe10c18a00
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2009-03-23 10:24:45 +0000

    Fixed workflows so that owners of a workspace can control push access to their repositories.
    Progress towards:
    https://tracker.physiomeproject.org/show_bug.cgi?id=1506
    
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@3293 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/app/security/roles.py
+++ pmr2/app/security/roles.py
@@ -1,5 +1,7 @@
 from zope.interface import implements 
 from plone.app.workflow.interfaces import ISharingPageRole 
+from plone.app.workflow.permissions import DelegateContributorRole
+from plone.app.workflow.permissions import DelegateReviewerRole
 
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory("pmr2")
@@ -7,7 +9,16 @@ _ = MessageFactory("pmr2")
 import pmr2.app.security
 
 
+# XXX required_permission is borrowed from plone.app.workflow for now.
 class WorkspacePusherRole(object):
     implements(ISharingPageRole)
     title = _(u'title_can_push', default=u'Can hg push')
-    required_permission = 'Mercurial Push'
+    #required_permission = 'Mercurial Push'
+    required_permission = DelegateContributorRole
+
+
+class ExposureCuratorRole(object):
+    implements(ISharingPageRole)
+    title = _(u'title_can_curate', default=u'Can curate')
+    #required_permission = 'Curate Exposure'
+    required_permission = DelegateReviewerRole
