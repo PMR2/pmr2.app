75ec209e7a8036196a7045762d5c604d2e70918c
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2009-12-23 03:01:45 +0000

    Removing PMR2 v0.1 rushed compatability for PMR1.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@4430 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/app/content.py
+++ pmr2/app/content.py
@@ -17,13 +17,6 @@ from pmr2.app._content.exposure import Exposure
 from pmr2.app._content.exposure import ExposureFile
 from pmr2.app._content.exposure import ExposureFolder
 
-# legacy Exposure types
-from pmr2.app._content.exposure import ExposureDocument
-from pmr2.app._content.exposure import ExposureMathDocument
-from pmr2.app._content.exposure import ExposureCmetaDocument
-from pmr2.app._content.exposure import ExposureCodeDocument
-from pmr2.app._content.exposure import ExposurePMR1Metadoc
-
 from pmr2.app._content.support import PMR2Search
 
 
@@ -32,12 +25,6 @@ atapi.registerType(ExposureContainer, 'pmr2.app')
 atapi.registerType(Exposure, 'pmr2.app')
 atapi.registerType(ExposureFile, 'pmr2.app')
 atapi.registerType(ExposureFolder, 'pmr2.app')
-
-atapi.registerType(ExposureDocument, 'pmr2.app')
-atapi.registerType(ExposureMathDocument, 'pmr2.app')
-atapi.registerType(ExposureCmetaDocument, 'pmr2.app')
-atapi.registerType(ExposureCodeDocument, 'pmr2.app')
-atapi.registerType(ExposurePMR1Metadoc, 'pmr2.app')
 
 def catalog_content(obj, event):
     # for the subscriber event.
@@ -50,93 +37,3 @@ def recursive_recatalog_content(obj, event):
     if IExposure.providedBy(obj) or IExposureFolder.providedBy(obj):
         for id_, subobj in obj.items():
             recursive_recatalog_content(subobj, event)
-
-
-class BaseDocumentFactory(object):
-    """\
-    The base document factory for exposure.
-    """
-
-    def __call__(self, filename):
-        name = str(filename + self.suffix)
-        obj = self.klass(oid=name)
-        # XXX filename conversion to unicode
-        obj.origin = unicode(filename)
-        return obj
-
-
-class BaseExposureDocumentFactory(BaseDocumentFactory):
-    """\
-    The base ExposureDocument factory.
-    """
-
-    def __call__(self, filename):
-        obj = super(BaseExposureDocumentFactory, self).__call__(filename)
-        obj.transform = self.transform
-        return obj
-
-
-class BaseExposureMetadocFactory(BaseDocumentFactory):
-    """\
-    The base ExposureMetadoc factory.
-    """
-
-    def __call__(self, filename):
-        obj = super(BaseExposureMetadocFactory, self).__call__(filename)
-        obj.factories = self.factories
-        return obj
-
-
-class ExposurePMR1DocumentFactory(BaseExposureDocumentFactory):
-
-    zope.interface.implements(IExposureDocumentFactory)
-
-    klass = ExposureDocument
-    description = u'PMR1 CellML Docbook Page'
-    suffix = u'.pmr1.html'
-    transform = u'pmr2_processor_legacy_tmpdoc2html'
-
-
-class ExposureMathDocumentFactory(BaseExposureDocumentFactory):
-
-    zope.interface.implements(IExposureDocumentFactory)
-
-    klass = ExposureMathDocument
-    description = u'PMR1 MathML Page'
-    suffix = u'.mathml.html'
-    transform = u'pmr2_processor_legacy_cellml2html_mathml'
-
-
-class ExposureCmetaDocumentFactory(BaseExposureDocumentFactory):
-
-    zope.interface.implements(IExposureDocumentFactory)
-
-    klass = ExposureCmetaDocument
-    description = u'CellML Metadata Page'
-    suffix = u'.pmr2.cmeta'
-    transform = u'pmr2_cellml_metadata'  # this needed?
-
-
-class ExposureCodeDocumentFactory(BaseExposureDocumentFactory):
-
-    zope.interface.implements(IExposureDocumentFactory)
-
-    klass = ExposureCodeDocument
-    description = u'CellML Code Generation Page'
-    suffix = u'.code.html'
-    transform = u'pmr2_processor_cellmlapi_cellml2c'
-
-
-class ExposurePMR1MetadocFactory(BaseExposureMetadocFactory):
-
-    zope.interface.implements(IExposureMetadocFactory)
-
-    klass = ExposurePMR1Metadoc
-    description = u'PMR1 Style Exposure'
-    suffix = u'.index.html'
-    factories = [
-        u'ExposurePMR1DocumentFactory',
-        u'ExposureMathDocumentFactory',
-        u'ExposureCmetaDocumentFactory',
-        u'ExposureCodeDocumentFactory',
-    ]
