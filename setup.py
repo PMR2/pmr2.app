6eb3335b67fb8a37146d0c8487c1b86c4b903367
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-01-15 03:01:30 +0000

    Setup script now takes version id from pmr2/app/version.txt.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@4481 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.0'
+version = open(os.path.join('pmr2', 'app', 'version.txt')).read().strip()
 
 setup(name='pmr2.app',
       version=version,
