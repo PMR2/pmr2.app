e6fe8dfd43204718030a6d3e5a56b4b5b8d18328
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-03-18 22:21:53 +0000

    Moved the content subscribers to the subscriber module.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@4797 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/app/content/__init__.py
+++ pmr2/app/content/__init__.py
@@ -2,9 +2,6 @@ import zope.interface
 
 from Products.Archetypes import atapi
 
-from pmr2.app.interfaces import *
-from pmr2.app.content.interfaces import *
-
 from pmr2.app.content.root import PMR2
 
 from pmr2.app.content.workspace import WorkspaceContainer
@@ -27,18 +24,6 @@ atapi.registerType(Exposure, 'pmr2.app')
 atapi.registerType(ExposureFile, 'pmr2.app')
 atapi.registerType(ExposureFolder, 'pmr2.app')
 
-def catalog_content(obj, event):
-    # for the subscriber event.
-    obj.reindexObject()
-
-def recursive_recatalog_content(obj, event):
-    # for exposure state changes.
-    # we are going to be restrictive in what we do.
-    obj.reindexObject()
-    if IExposure.providedBy(obj) or IExposureFolder.providedBy(obj):
-        for id_, subobj in obj.items():
-            recursive_recatalog_content(subobj, event)
-
 __all__ = [
     'PMR2',
     'WorkspaceContainer',
@@ -50,6 +35,4 @@ __all__ = [
     'ExposureFile',
     'ExposureFolder',
     'PMR2Search',
-    'catalog_content',
-    'recursive_recatalog_content',
 ]
--- pmr2/app/subscriber.py
+++ pmr2/app/subscriber.py
@@ -1,7 +1,19 @@
 from zope.component import queryUtility, getSiteManager
 
 from pmr2.app.interfaces import IPMR2GlobalSettings
+from pmr2.app.content.interfaces import IExposure, IExposureFolder
 
+def catalog_content(obj, event):
+    # for the subscriber event.
+    obj.reindexObject()
+
+def recursive_recatalog_content(obj, event):
+    # for exposure state changes.
+    # we are going to be restrictive in what we do.
+    obj.reindexObject()
+    if IExposure.providedBy(obj) or IExposureFolder.providedBy(obj):
+        for id_, subobj in obj.items():
+            recursive_recatalog_content(subobj, event)
 
 def create_user_workspace(user, event):
     """\
