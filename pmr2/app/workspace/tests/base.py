a3b1a776799dec2020a077a7be6c6292ed0d3c11
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-10-20 07:50:03 +0000

    Remove the now unneeded initialization of test with Mercurial.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@5365 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/app/workspace/tests/base.py
+++ pmr2/app/workspace/tests/base.py
@@ -47,30 +47,9 @@ class WorkspaceDocTestCase(DocTestCase):
         from pmr2.app.tests import utils
         from pmr2.app.interfaces import IPMR2GlobalSettings
         setup_defaults()
+        # also set up the root to the tmpdir.
         self.pmr2 = zope.component.getUtility(IPMR2GlobalSettings)
-        # force the repo_root unset
         self.pmr2.repo_root = self.tmpdir
-
-    def createRepo(self):
-        # create real Hg repos, to be called only after workspace is
-        # created and model root path is assigned
-        import pmr2.mercurial.tests
-        from pmr2.app.workspace.content import Workspace
-        from pmr2.mercurial.tests import util
-        p = self.pmr2.createDir(self.portal.workspace)
-        util.extract_archive(p)
-        # pmr2.app
-        # XXX Mercurial specific
-        p2a_test = join(dirname(pmr2.testing.__file__), 'pmr2.app.testdata.tgz')
-        util.extract_archive(p, p2a_test)
-        self.portal.workspace['pmr2hgtest'] = Workspace('pmr2hgtest')
-        self.portal.workspace['rdfmodel'] = Workspace('rdfmodel')
-        self.pmr2hgtest_revs = util.ARCHIVE_REVS
-        self.rdfmodel_revs = [
-            'b94d1701154be42acf63ee6b4bd4a99d09ba043c',
-            '2647d4389da6345c26d168bbb831f6512322d4f9',
-            '006f11cd9211abd2a879df0f6c7f27b9844a8ff2',
-        ]
 
     def tearDown(self):
         DocTestCase.tearDown(self)
