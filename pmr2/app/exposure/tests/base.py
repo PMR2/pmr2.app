44fe723c044bf896c8baedf6d8ca248d864b116b
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2011-07-05 06:13:51 +0000

    Made the id related test more flexible.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.app/trunk@5918 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/app/exposure/tests/base.py
+++ pmr2/app/exposure/tests/base.py
@@ -41,7 +41,7 @@ class ExposureDocTestCase(WorkspaceDocTestCase):
 
         super(ExposureDocTestCase, self).setUp()
 
-        self.pmr2.default_exposure_idgen = 'rand128hex'
+        self.pmr2.default_exposure_idgen = 'autoinc'
         self.pmr2.repo_root = self.tmpdir
 
         from pmr2.app.workspace.content import WorkspaceContainer, Workspace
@@ -77,6 +77,9 @@ class CompleteDocTestCase(ExposureDocTestCase):
         def mkexposure(workspace, commit_id, id_):
             if not id_:
                 id_ = idgen.next()
+            else:
+                # call it anyway.
+                idgen.next()
             e = Exposure(id_)
             e.workspace = workspace
             e.commit_id = commit_id
