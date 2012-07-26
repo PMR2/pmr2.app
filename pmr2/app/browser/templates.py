01ee1b564d5ded0562e03c55c93c0cdd163c36e3
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-07-26 18:56:35 +1200

    Move module specific settings into right places.
      - Exposure and Workspace browser macro zcml definitions now placed at
        where one might expect them

--- pmr2/app/browser/templates.py
+++ pmr2/app/browser/templates.py
@@ -1,3 +1,14 @@
 import os.path
 
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
+from plone.app.z3cform import templates
+
 path = lambda p: os.path.join(os.path.dirname(__file__), 'templates', p)
+
+
+class Macros(templates.Macros):
+    """\
+    Extension to the default macros.
+    """
+
+    index = ViewPageTemplateFile(path('macros.pt'))
