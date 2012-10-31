209f1f6f6085b4cf8850fc806ae1487345b48545
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-10-31 15:06:48 +1300

    Need to remove the template definitions also.

--- pmr2/app/browser/templates.py
+++ pmr2/app/browser/templates.py
@@ -5,21 +5,3 @@ from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 path = lambda p: os.path.join(os.path.dirname(__file__), 'templates', p)
 
 from plone.app.z3cform.templates import Macros
-
-from plone.app.z3cform import templates
-
-
-class PMR2PlainMacros(templates.Macros):
-    """\
-    Trying to make life easier for general testing
-    """
-
-    index = ViewPageTemplateFile(path('pmr2-plain-macros.pt'))
-
-
-class PMR2MainMacros(templates.Macros):
-    """\
-    The main macros, including templates.
-    """
-
-    index = ViewPageTemplateFile(path('pmr2-main-macros.pt'))
