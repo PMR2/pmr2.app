5d6fa4891c21e7a2750443f51d3fa1b6ae73886a
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-10-31 16:48:05 +1300

    Finally remove the remainder of the macros.

--- pmr2/app/browser/templates.py
+++ pmr2/app/browser/templates.py
@@ -1,7 +1,3 @@
 import os.path
-import zope.deprecation
-from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 
 path = lambda p: os.path.join(os.path.dirname(__file__), 'templates', p)
-
-from plone.app.z3cform.templates import Macros
