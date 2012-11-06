b10448ff0017ad3707473598ac473dbb63367441
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-11-06 14:17:32 +1300

    Disable the default adding page for Exposure.
      - This was really confusing users.  A somewhat helpful message now
        replaces the default CMF adding form.
    
    Progress towards: tracker item 3459

---
+++ pmr2/app/exposure/browser/adding.py
@@ -0,0 +1,13 @@
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
+
+from pmr2.z3cform import page
+
+from pmr2.app.exposure.browser.templates import path
+
+
+class ExposureCreateInterceptPage(page.SimplePage):
+    """\
+    Direct users to the proper way to create exposures.
+    """
+
+    template = ViewPageTemplateFile(path('exposure_create.pt'))
