df1da1755328b1b4486a21590b80f5b0e1c1fd2c
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-03-14 17:37:54 +1300

    Move more of the setup to the base setup.
      - Instead of having it in the doctest file.

--- pmr2/app/workspace/tests/base.py
+++ pmr2/app/workspace/tests/base.py
@@ -87,3 +87,17 @@ class WorkspaceDocTestCase(DocTestCase):
 
     def tearDown(self):
         DocTestCase.tearDown(self)
+
+
+class WorkspaceBrowserDocTestCase(WorkspaceDocTestCase):
+
+    def setUp(self):
+        super(WorkspaceBrowserDocTestCase, self).setUp()
+        from pmr2.app.workspace.content import Workspace, WorkspaceContainer
+        self.portal['workspace'] = WorkspaceContainer()
+
+        self.portal.workspace['test'] = Workspace('test')
+        self.portal.workspace.test.storage = u'dummy_storage'
+
+        self.portal.workspace['test2'] = Workspace('test2')
+        self.portal.workspace.test2.storage = u'dummy_storage'
