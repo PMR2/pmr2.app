b09344075cdd0859b090e005b32d4b9c2d79b7c4
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-06-17 16:32:52 +1200

    Define the custom add exposure content options
    - As we don't want users to accidentally trigger the ATCT add process
      which can cause useless objects to spawn, define the add views and
      expressions to point to them.
    
    Fixes tracker item 3340

--- pmr2/app/exposure/browser/adding.py
+++ pmr2/app/exposure/browser/adding.py
@@ -12,3 +12,30 @@ class ExposureCreateInterceptPage(page.SimplePage):
 
     label = u'Create Exposure'
     template = ViewPageTemplateFile(path('exposure_create.pt'))
+
+
+class ExposureFileCreateInterceptPage(page.SimplePage):
+    """\
+    Direct users to the proper way to create exposures.
+    """
+
+    label = u'Create Exposure File'
+    template = ViewPageTemplateFile(path('exposure_f_create.pt'))
+
+
+class ExposureFolderCreateInterceptPage(page.SimplePage):
+    """\
+    Direct users to the proper way to create exposures.
+    """
+
+    label = u'Create Exposure Folder'
+    template = ViewPageTemplateFile(path('exposure_f_create.pt'))
+
+
+class ExposureContainerCreateInterceptPage(page.SimplePage):
+    """\
+    Direct users to the proper way to create exposures.
+    """
+
+    label = u'Create Exposure Container'
+    template = ViewPageTemplateFile(path('exposure_container_create.pt'))
