519248e959f62d7302a45c996e1b9d95d5bcdf72
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-08-21 18:40:16 +1200

    Force the Exposure workflow state menu to render
    - The last fix was not strong enough - workflow states could prevent
      access to the wizard form.
    - However requires use of overrides.  We try to limit the potential
      damage by enforcing a check of the permissions.

--- pmr2/app/exposure/browser/menu.py
+++ pmr2/app/exposure/browser/menu.py
@@ -1,7 +1,38 @@
+import zope.component
 from zope.browsermenu.menu import BrowserSubMenuItem
+
+from plone.app.contentmenu.menu import WorkflowMenu
+from plone.app.contentmenu.menu import WorkflowSubMenuItem
+
+from pmr2.app.exposure.interfaces import IExposureSourceAdapter
+from pmr2.app.exposure.interfaces import IExposureFile
+from pmr2.app.exposure.interfaces import IExposureFolder
 
 
 class NullSubMenuItem(BrowserSubMenuItem):
 
     def available(self):
         return False
+
+
+class ExposureWorkflowMenu(WorkflowMenu):
+
+    def getMenuItems(self, context, request):
+        # Have to set the context to the root.
+        if (IExposureFile.providedBy(context) or
+                IExposureFolder.providedBy(context)):
+            helper = zope.component.queryAdapter(
+                context, IExposureSourceAdapter)
+            exposure, workspace, path = helper.source()
+            context = exposure
+        return WorkflowMenu.getMenuItems(self, context, request)
+
+
+class ExposureWorkflowSubMenuItem(WorkflowSubMenuItem):
+
+    def __init__(self, context, request):
+        # Have to set the context to the root.
+        helper = zope.component.queryAdapter(context, IExposureSourceAdapter)
+        exposure, workspace, path = helper.source()
+        context = exposure
+        WorkflowSubMenuItem.__init__(self, context, request)
